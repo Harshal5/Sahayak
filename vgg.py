ceefb50f392e5ba163874aab835684f93d90536e
Harshal5 <harshalnpatil5@gmail.com>
2021-04-16 09:11:13 +0530

    vgg16 + head_model

---
+++ vgg.py
@@ -0,0 +1,55 @@
+import  os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input,Flatten,Dense,Dropout,Conv2D,MaxPooling2D,GlobalAveragePooling2D
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.applications.vgg16 import preprocess_input,decode_predictions
+
+path_root = "./dataset"
+train_path = os.path.join(path_root, "train")
+test_path = os.path.join(path_root, "test")
+
+data_gen_train = ImageDataGenerator(rescale=1./255)
+data_gen_valid = ImageDataGenerator(rescale=1./255)
+
+train_generator = data_gen_train.flow_from_directory(
+	train_path,
+	target_size=(128, 128),
+	class_mode='categorical',
+    batch_size=256
+)
+
+validation_generator = data_gen_valid.flow_from_directory(
+	test_path,
+	target_size=(128, 128),
+	class_mode='categorical',
+    batch_size=256
+)
+
+image_shape = (128, 128, 3)
+
+
+def create_model():
+	base_model = tf.keras.applications.VGG16(weights='imagenet', input_shape=image_shape, include_top=False)
+	base_model.trainable = False
+	# print(base_model.summary())
+
+	head_model = base_model.output
+	head_model = GlobalAveragePooling2D()(head_model)
+	head_model = Flatten(name="flatten")(head_model)
+	head_model = Dense(128, activation="relu")(head_model)
+	head_model = Dropout(0.3)(head_model)
+	head_model = Dense(35, activation="softmax")(head_model)
+	# print(head_model.summary()
+	model = Model(inputs=base_model.input, outputs=head_model)
+	return model
+
+model = create_model()
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+epochs = 10
+model.save('./my_vggmodel.h5')
+H = model.fit(train_generator, validation_data=validation_generator,epochs=epochs,verbose=1)
+score = model.evaluate(validation_generator)
+print(score)
