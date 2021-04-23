75059b029e1e97c463f74a0db9151eba0b25d118
Sarvesh Bodakhe <sarveshbodakhe14@gmail.com>
2021-04-24 01:37:15 +0530

    ui improvements in text-to-gesture

--- src/components/CarouselCardItem.js
+++ src/components/CarouselCardItem.js
@@ -1,68 +1,59 @@
 import React from 'react'
 import { View, Text, StyleSheet, Dimensions, Image, Button } from "react-native"
 
-// export const SLIDER_WIDTH = Dimensions.get('window').width + 80
 export const SLIDER_WIDTH = Dimensions.get('window').width
 export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
 
 const CarouselCardItem = ({ item, index }) => {
     return (
         <View style={styles.container} key={index}>
-            {/* <Image
-                source={{ uri: item.imageUri }}
-                // source={require(item.imageUri)}
-                style={styles.image}
-            /> */}
             {item.imageComponent}
 
-
-            <Text style={styles.header}>{item.char}</Text>
-            {/* <Text style={styles.body}>{item.index}</Text> */}
+            <View style={styles.textBox}>
+                <Text style={styles.header}>{item.char}</Text>
+            </View>
         </View>
     )
 }
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'column',
-        // alignContent: 'center',
         alignItems: 'center',
-        // alignSelf: 'center',
-        // justifyContent: 'center',
-        backgroundColor: "#ff4d4d",
-        // alignContent: 'center',
-        // alignSelf: 'stretch',
-        // borderRadius: 8,
+        backgroundColor: "#d9d9d9", // WHITE
         width: ITEM_WIDTH,
-        paddingBottom: 40,
+        borderRadius: 20,
+        width: '100%',
+        height: 300,
         shadowColor: "#000",
         shadowOffset: {
-            width: 0,
+            width: 1,
             height: 3,
         },
-        shadowOpacity: 0.29,
-        shadowRadius: 4.65,
+        shadowOpacity: 5.29,
+        shadowRadius: 5.65,
         elevation: 7,
-        marginHorizontal: 0,
     },
     image: {
-        width: ITEM_WIDTH,
-        height: 300,
     },
     header: {
-        color: "#222",
+        color: "#ffffff",
         fontSize: 28,
         fontWeight: "bold",
-        // paddingLeft: 20,
-        paddingTop: 20,
         justifyContent: 'center',
     },
-    body: {
-        color: "#222",
-        fontSize: 18,
-        paddingLeft: 20,
-        paddingLeft: 20,
-        paddingRight: 20
+    textBox: {
+        alignItems: 'center',
+        backgroundColor: "#b0b0b0",
+        height: 40,
+        width: '75%',
+        borderBottomEndRadius: 30,
+        borderTopEndRadius: 30,
+        borderBottomStartRadius: 30,
+        borderTopLeftRadius: 30,
+
     }
+
+
 })
 
 export default CarouselCardItem
--- src/components/StringImages.jsx
+++ src/components/StringImages.jsx
@@ -7,108 +7,45 @@ import { Title, TextInput, Button, List, Avatar, Card, Paragraph } from 'react-n
 
 const getImage = (char) => {
     switch (char) {
-        case 'a': return <Image source={require('../../assets/img/a.png')} />
-        case 'b': return <Image source={require('../../assets/img/b.png')} />
-        case 'c': return <Image source={require('../../assets/img/c.png')} />
-        case 'd': return <Image source={require('../../assets/img/d.png')} />
-        case 'i': return <Image source={require('../../assets/img/i.png')} />
-        case 'p': return <Image source={require('../../assets/img/p.png')} />
-        case 'q': return <Image source={require('../../assets/img/q.png')} />
-        case 't': return <Image source={require('../../assets/img/t.png')} />
-        case 'n': return <Image source={require('../../assets/img/n.png')} />
-        case 'w': return <Image source={require('../../assets/img/w.png')} />
-        case 'x': return <Image source={require('../../assets/img/x.png')} />
-        case 'y': return <Image source={require('../../assets/img/y.png')} />
-        case 'o': return <Image source={require('../../assets/img/o.png')} />
-        case 'z': return <Image source={require('../../assets/img/z.png')} />
-        case '1': return <Image source={require('../../assets/img/1.png')} />
-        case '2': return <Image source={require('../../assets/img/2.png')} />
-        case '4': return <Image source={require('../../assets/img/4.png')} />
-        case '5': return <Image source={require('../../assets/img/5.png')} />
-        case '7': return <Image source={require('../../assets/img/7.png')} />
-        case '9': return <Image source={require('../../assets/img/9.png')} />
-        case 'f': return <Image source={require('../../assets/img/f.png')} />
-        case 'g': return <Image source={require('../../assets/img/g.png')} />
-        case 'm': return <Image source={require('../../assets/img/m.png')} />
-        default: return <Image style={{ height: 200, width: 200 }} source={require('../../assets/img/not-found.jpg')} />
+        case 'a': return <Image style={styles2.img} source={require('../../assets/img/a.png')} />
+        case 'b': return <Image style={styles2.img} source={require('../../assets/img/b.png')} />
+        case 'c': return <Image style={styles2.img} source={require('../../assets/img/c.png')} />
+        case 'd': return <Image style={styles2.img} source={require('../../assets/img/d.png')} />
+        case 'i': return <Image style={styles2.img} source={require('../../assets/img/i.png')} />
+        case 'p': return <Image style={styles2.img} source={require('../../assets/img/p.png')} />
+        case 'q': return <Image style={styles2.img} source={require('../../assets/img/q.png')} />
+        case 't': return <Image style={styles2.img} source={require('../../assets/img/t.png')} />
+        case 'n': return <Image style={styles2.img} source={require('../../assets/img/n.png')} />
+        case 'w': return <Image style={styles2.img} source={require('../../assets/img/w.png')} />
+        case 'x': return <Image style={styles2.img} source={require('../../assets/img/x.png')} />
+        case 'y': return <Image style={styles2.img} source={require('../../assets/img/y.png')} />
+        case 'o': return <Image style={styles2.img} source={require('../../assets/img/o.png')} />
+        case 'z': return <Image style={styles2.img} source={require('../../assets/img/z.png')} />
+        case '1': return <Image style={styles2.img} source={require('../../assets/img/1.png')} />
+        case '2': return <Image style={styles2.img} source={require('../../assets/img/2.png')} />
+        case '4': return <Image style={styles2.img} source={require('../../assets/img/4.png')} />
+        case '5': return <Image style={styles2.img} source={require('../../assets/img/5.png')} />
+        case '7': return <Image style={styles2.img} source={require('../../assets/img/7.png')} />
+        case '9': return <Image style={styles2.img} source={require('../../assets/img/9.png')} />
+        case 'f': return <Image style={styles2.img} source={require('../../assets/img/f.png')} />
+        case 'g': return <Image style={styles2.img} source={require('../../assets/img/g.png')} />
+        case 'm': return <Image style={styles2.img} source={require('../../assets/img/m.png')} />
+        default: return <Image style={styles2.img} source={require('../../assets/img/not-found.jpg')} />
     }
 }
 
-const getImageUri = (char) => {
-    switch (char) {
-        case 'a': return require('../../assets/img/a.png')
-        case 'b': return require('../../assets/img/b.png')
-        case 'c': return require('../../assets/img/c.png')
-        case 'd': return require('../../assets/img/d.png')
-        case 'i': return require('../../assets/img/i.png')
-        case 'p': return require('../../assets/img/p.png')
-        case 'q': return require('../../assets/img/q.png')
-        case 't': return require('../../assets/img/t.png')
-        case 'n': return require('../../assets/img/n.png')
-        case 'w': return require('../../assets/img/w.png')
-        case 'x': return require('../../assets/img/x.png')
-        case 'y': return require('../../assets/img/y.png')
-        case 'o': return require('../../assets/img/o.png')
-        case 'z': return require('../../assets/img/z.png')
-        case '1': return require('../../assets/img/1.png')
-        case '2': return require('../../assets/img/2.png')
-        case '4': return require('../../assets/img/4.png')
-        case '5': return require('../../assets/img/5.png')
-        case '7': return require('../../assets/img/7.png')
-        case '9': return require('../../assets/img/9.png')
-        case 'f': return require('../../assets/img/f.png')
-        case 'g': return require('../../assets/img/g.png')
-        case 'm': return require('../../assets/img/m.png')
-        default: return require('../../assets/img/not-found.jpg')
+const styles2 = StyleSheet.create({
+    img: {
+        margin: 20,
+        height: 200,
+        width: 200,
+        borderRadius: 40,
     }
-    return require('../../assets/img/not-found.jpg')
-}
+})
 
 
-const CharCard = (props) => {
-    console.log("props.item:", props.item);
-    const getPath = (item) => {
-        switch (item) {
-            case 'a': return <Image source={require('../../assets/img/a.png')} />
-            case 'b': return <Image source={require('../../assets/img/b.png')} />
-            case 'c': return <Image source={require('../../assets/img/c.png')} />
-            case 'd': return <Image source={require('../../assets/img/d.png')} />
-            case 'i': return <Image source={require('../../assets/img/i.png')} />
-            case 'p': return <Image source={require('../../assets/img/p.png')} />
-            case 'q': return <Image source={require('../../assets/img/q.png')} />
-            case 't': return <Image source={require('../../assets/img/t.png')} />
-            case 'n': return <Image source={require('../../assets/img/n.png')} />
-            case 'w': return <Image source={require('../../assets/img/w.png')} />
-            case 'x': return <Image source={require('../../assets/img/x.png')} />
-            case 'y': return <Image source={require('../../assets/img/y.png')} />
-            case 'o': return <Image source={require('../../assets/img/o.png')} />
-            case 'z': return <Image source={require('../../assets/img/z.png')} />
-            case '1': return <Image source={require('../../assets/img/1.png')} />
-            case '2': return <Image source={require('../../assets/img/2.png')} />
-            case '4': return <Image source={require('../../assets/img/4.png')} />
-            case '5': return <Image source={require('../../assets/img/5.png')} />
-            case '7': return <Image source={require('../../assets/img/7.png')} />
-            case '9': return <Image source={require('../../assets/img/9.png')} />
-            case 'f': return <Image source={require('../../assets/img/f.png')} />
-            case 'g': return <Image source={require('../../assets/img/g.png')} />
-            case 'm': return <Image source={require('../../assets/img/m.png')} />
-            default: return <Image style={{ height: 200, width: 200 }} source={require('../../assets/img/not-found.jpg')} />
-        }
-    }
-    return (
-
-        <Card>
-            <Card.Title title={props.item} />
-            <Card.Content>
-                {getPath(props.item)}
-            </Card.Content>
-        </Card>
-
-    );
-}
-
-import Carousel, { Pagination } from 'react-native-snap-carousel'
-import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from '../components/CarouselCardItem'
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from '../components/CarouselCardItem';
 
 const CarouselCards = (props) => {
     const isCarousel = React.useRef(null);
@@ -151,25 +88,12 @@ const CarouselCards = (props) => {
 
 const StringImages = (props) => {
     var arr = props.text.split('');
-
     const data = arr.map((val, index) => {
-        return ({ char: val, index: index, imageComponent: getImage(val), imageUri: getImageUri(val) })
+        return ({ char: val, index: index, imageComponent: getImage(val) })
     })
-    // console.log("data:", data);
     return (
-        // <View>
-        //     <FlatList
-        //         data={arr}
-        //         renderItem={CharCard}
-        //     // key={count++}
-        //     // keyExtractor={() => Math.floor((new Date()).getTime() / 1000 + count++)}
-        //     />
-        // </View>
-
-        <View>
-            {/* <SafeAreaView style={styles.container}> */}
+        <View style={styles.container}>
             <CarouselCards data={data} />
-            {/* </SafeAreaView > */}
         </View>
     );
 
@@ -177,18 +101,12 @@ const StringImages = (props) => {
 
 const styles = StyleSheet.create({
     container: {
-        // backgroundColor: "#00ffcc", //Blue
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        // padding: 50
+        height: 400,
+        paddingVertical: 20,
     },
     CarouselCardsContainer: {
-        // backgroundColor: "#ffbf80", //Yello
+        height: 400,
     },
-    Carousel: {
-        backgroundColor: "#00ffcc" //red
-    }
 });
 
 export default StringImages;
