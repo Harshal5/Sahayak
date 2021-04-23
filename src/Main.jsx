b63d8d06a0e9c74a43469aad975ae165b938cc71
kanhaiya38 <kanhaiya38@protonmail.com>
2021-04-23 19:20:16 +0530

    minor change

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
@@ -8,9 +8,6 @@ import {
   DarkTheme as PaperDarkTheme,
   DefaultTheme as PaperDefaultTheme,
 } from 'react-native-paper';
-import { Entypo } from '@expo/vector-icons';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
 import { useColorScheme } from 'react-native-appearance';
 import merge from 'deepmerge';
 
@@ -24,7 +21,6 @@ const CombinedDefaultTheme = merge(
 const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
 
 const Main = () => {
-  const [appIsReady, setAppIsReady] = useState(false);
   const colorScheme = useColorScheme();
   const [isThemeDark, setIsThemeDark] = React.useState(
     colorScheme === 'dark',
@@ -45,40 +41,63 @@ const Main = () => {
     }),
     [toggleTheme, isThemeDark],
   );
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Keep the splash screen visible while we fetch resources
-        await SplashScreen.preventAutoHideAsync();
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(Entypo.font);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
-        await new Promise((resolve) => setTimeout(resolve, 4000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
 
-    prepare();
-  }, []);
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
+  //   return (
+  //     <PreferencesContext.Provider value={preferences}>
+  //       <PaperProvider theme={theme}>
+  //         <NavigationContainer theme={theme}>
+  //           <Drawer.Navigator
+  //             drawerContent={DrawerContent}
+  //             initialRouteName="Home"
+  //           >
+  //             <Drawer.Screen
+  //               name="Home"
+  //               component={HomeTabScreen}
+  //               options={{
+  //                 drawerLabel: 'Home',
+  //                 drawerIcon: ({ color }) => (
+  //                   <MaterialCommunityIcons
+  //                     name="home"
+  //                     color={color}
+  //                     size={22}
+  //                   />
+  //                 ),
+  //               }}
+  //             />
+  //             <Drawer.Screen
+  //               name="Contact Us"
+  //               component={ContactStackScreen}
+  //               options={{
+  //                 drawerLabel: 'Contact Us',
+  //                 drawerIcon: ({ color }) => (
+  //                   <MaterialCommunityIcons
+  //                     name="email"
+  //                     color={color}
+  //                     size={22}
+  //                   />
+  //                 ),
+  //               }}
+  //             />
+  //             <Drawer.Screen
+  //               name="Settings"
+  //               component={SettingsStackScreen}
+  //               options={{
+  //                 drawerLabel: 'Settings',
+  //                 drawerIcon: ({ color }) => (
+  //                   <MaterialCommunityIcons
+  //                     name="cog"
+  //                     color={color}
+  //                     size={22}
+  //                   />
+  //                 ),
+  //               }}
+  //             />
+  //           </Drawer.Navigator>
+  //         </NavigationContainer>
+  //       </PaperProvider>
+  //     </PreferencesContext.Provider>
+  //   );
+  // };
   return (
     <PreferencesContext.Provider value={preferences}>
       <PaperProvider theme={theme}>
